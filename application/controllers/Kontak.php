57d9b7af1d4178aeb7e122f73a999010fe67fc19
mrrest017 <55204773+mrrest017@users.noreply.github.com>
2020-05-13 10:35:03 +0700

    www

--- application/controllers/Kontak.php
+++ application/controllers/Kontak.php
@@ -14,12 +14,12 @@ class Kontak extends REST_Controller {
 
     //Menampilkan data kontak
     function index_get() {
-        $id = $this->get('id');
+        $id = $this->get('nik');
         if ($id == '') {
-            $kontak = $this->db->get('telepon')->result();
+            $kontak = $this->db->get('tb_ktp')->result();
         } else {
-            $this->db->where('id', $id);
-            $kontak = $this->db->get('telepon')->result();
+            $this->db->where('nik', $id);
+            $kontak = $this->db->get('tb_ktp')->result();
         }
         $this->response($kontak, 200);
     }
@@ -27,10 +27,24 @@ class Kontak extends REST_Controller {
     //Mengirim atau Menambah data Kontak
     function index_post() {
         $data = array(
-                    'id'       => $this->post('id'),
-                    'nama'     => $this->post('nama'),
-                    'nomor'    => $this->post('nomor'));
-        $insert = $this->db->insert('telepon', $data);
+                    'nik'               => $this->post('nik'),
+                    'nama'              => $this->post('nama'),
+                    'tempatLahir'       => $this->post('tempatLahir'),
+                    'tanggalLahir'      => $this->post('tanggalLahir'),
+                    'jenisKelamin'      => $this->post('jenisKelamin'),
+                    'golDarah'          => $this->post('golDarah'),
+                    'alamat'            => $this->post('alamat'),
+                    'kodeRt'            => $this->post('kodeRt'),
+                    'kodeRw'            => $this->post('kodeRw'),
+                    'kelurahan'         => $this->post('kelurahan'),
+                    'kecamatan'         => $this->post('kecamatan'),
+                    'agama'             => $this->post('agama'),
+                    'statusPerkawinan'  => $this->post('statusPerkawinan'),
+                    'pekerjaan'         => $this->post('pekerjaan'),
+                    'kewarganegaraan'   => $this->post('kewarganegaraan'),
+                    'berlakuHingga'     => $this->post('berlakuHingga'),
+                    'gambar_ktp'        => $this->post('gambar_ktp'));
+        $insert = $this->db->insert('tb_ktp', $data);
         if ($insert) {
             $this->response($data, 200);
         } else {
@@ -42,11 +56,25 @@ class Kontak extends REST_Controller {
     function index_put() {
         $id = $this->put('id');
         $data = array(
-                    'id'       => $this->put('id'),
-                    'nama'          => $this->put('nama'),
-                    'nomor'    => $this->put('nomor'));
-        $this->db->where('id', $id);
-        $update = $this->db->update('telepon', $data);
+                    'nik'               => $this->post('nik'),
+                    'nama'              => $this->post('nama'),
+                    'tempatLahir'       => $this->post('tempatLahir'),
+                    'tanggalLahir'      => $this->post('tanggalLahir'),
+                    'jenisKelamin'      => $this->post('jenisKelamin'),
+                    'golDarah'          => $this->post('golDarah'),
+                    'alamat'            => $this->post('alamat'),
+                    'kodeRt'            => $this->post('kodeRt'),
+                    'kodeRw'            => $this->post('kodeRw'),
+                    'kelurahan'         => $this->post('kelurahan'),
+                    'kecamatan'         => $this->post('kecamatan'),
+                    'agama'             => $this->post('agama'),
+                    'statusPerkawinan'  => $this->post('statusPerkawinan'),
+                    'pekerjaan'         => $this->post('pekerjaan'),
+                    'kewarganegaraan'   => $this->post('kewarganegaraan'),
+                    'berlakuHingga'     => $this->post('berlakuHingga'),
+                    'gambar_ktp'        => $this->post('gambar_ktp'));
+        $this->db->where('nik', $id);
+        $update = $this->db->update('tb_ktp', $data);
         if ($update) {
             $this->response($data, 200);
         } else {
@@ -56,9 +84,9 @@ class Kontak extends REST_Controller {
 
     //Menghapus data yang sudah ada
     function index_delete() {
-        $id = $this->delete('id');
-        $this->db->where('id', $id);
-        $delete = $this->db->delete('telepon');
+        $id = $this->delete('nik');
+        $this->db->where('nik', $id);
+        $delete = $this->db->delete('tb_ktp');
         if ($delete) {
             $this->response(array('status' => 'success'), 201);
         } else {
